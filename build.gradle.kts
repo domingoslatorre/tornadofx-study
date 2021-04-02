7c0c67c557e20b659daa9d6bc2b4142d131b0306
Domingos Latorre <domingos.latorre@gmail.com>
2021-04-02 10:16:59 -0300

    Config tornadofx build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.4.31"
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
 group = "com.domingoslatorre"
@@ -11,4 +15,16 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("no.tornado:tornadofx:1.7.20")
+}
+
+javafx {
+    version = "11.0.2"
+    modules = mutableListOf("javafx.controls", "javafx.graphics")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
