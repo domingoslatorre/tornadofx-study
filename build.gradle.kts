e7f5d330992c5fcc9a0aa235603a0be93b980841
Domingos Latorre <domingos.latorre@gmail.com>
2021-04-02 10:03:03 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "com.domingoslatorre"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
