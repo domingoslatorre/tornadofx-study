3f8caa8a84f52f2032773600908a94dba5cbc80e
Domingos Latorre <domingos.latorre@gmail.com>
2021-04-04 10:45:01 -0300

    Add input to list itens example

--- src/main/kotlin/MyItensBind.kt
+++ src/main/kotlin/MyItensBind.kt
@@ -1,20 +1,43 @@
+import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import tornadofx.App
 import tornadofx.Controller
 import tornadofx.View
+import tornadofx.action
+import tornadofx.button
+import tornadofx.field
+import tornadofx.fieldset
+import tornadofx.form
 import tornadofx.label
 import tornadofx.launch
 import tornadofx.listview
+import tornadofx.textfield
 import tornadofx.vbox
 
 class MyItensController : Controller() {
     val values = FXCollections.observableArrayList("Alpha", "Beta", "Gamma", "Delta")
+    fun addToList(value: String) = run { values.add(value) }
 }
 
 class MyItensBindView : View() {
     val controller: MyItensController by inject()
+    val input = SimpleStringProperty()
 
     override val root = vbox {
+        form {
+            fieldset {
+                field("Input") {
+                    textfield(input)
+                }
+
+                button("Add") {
+                    action {
+                        controller.addToList(input.value)
+                        input.value = ""
+                    }
+                }
+            }
+        }
         label("My itens")
         listview(controller.values)
     }
